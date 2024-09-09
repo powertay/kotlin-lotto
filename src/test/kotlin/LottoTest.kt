5db65aca99ee44b8cf4ce68667584ba30f3ef145
mskangg <funnycoding91@gmail.com>
2024-09-09 14:08:09 +0900

    feat: 로또는 6개의 숫자를 갖고 있다.

--- src/test/kotlin/LottoTest.kt
+++ src/test/kotlin/LottoTest.kt
@@ -1,6 +1,14 @@
 import org.assertj.core.api.Assertions.assertThatIllegalStateException
 import org.junit.jupiter.api.Test
 
+class Lotto(
+    private val lottoNumbers: Set<LottoNumber>
+) {
+    init {
+        check(lottoNumbers.size == 6) { "로또는 6개의 숫자를 가져야 한다." }
+    }
+}
+
 class LottoNumber(
     private val number: Int
 ) {
@@ -17,4 +25,12 @@ class LottoTest {
             .isThrownBy { LottoNumber(46) }
             .withMessage("로또의 숫자는 1-45 여야 합니다.")
     }
+
+    @Test
+    fun `로또는 6개의 숫자를 갖고 있다`() {
+        assertThatIllegalStateException()
+            .isThrownBy { Lotto(setOf(LottoNumber(3))) }
+            .withMessage("로또는 6개의 숫자를 가져야 한다.")
+    }
+
 }
