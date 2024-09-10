7247269b3addbafc062a1822fe15409e9f4e4bb4
mskangg <funnycoding91@gmail.com>
2024-09-10 09:12:03 +0900

    feat: Lotto 부생성자 추가

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -10,4 +10,11 @@ class Lotto(
     init {
         check(lottoNumbers.size == 6) { "로또는 6개의 숫자를 가져야 한다." }
     }
+
+    constructor(lottoNumbers: List<Int>) : this(lottoNumbers.map { LottoNumber(it) }.toSet())
+    constructor(vararg lottoNumbers: Int) : this(lottoNumbers.toList())
+
+    fun contains(lottoNumber: LottoNumber): Boolean {
+        return lottoNumbers.contains(lottoNumber)
+    }
 }
--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -7,13 +7,13 @@ class LottoTest {
     @Test
     fun `로또는 6개의 숫자를 갖고 있다`() {
         assertThatIllegalStateException()
-            .isThrownBy { Lotto(setOf(LottoNumber(3))) }
+            .isThrownBy { Lotto(3) }
             .withMessage("로또는 6개의 숫자를 가져야 한다.")
     }
 
     @Test
     fun `로또번호가 포함되어있는지 확인한다`() {
-        val lotto = Lotto(listOf(1,2,3,4,5,6))
+        val lotto = Lotto(1, 2, 3, 4, 5, 6)
         val actual = lotto.contains(LottoNumber(1))
         assertThat(actual).isTrue()
     }
