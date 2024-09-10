81d1a3bc86ad826a3c63082804ea092fbad4d567
mskangg <funnycoding91@gmail.com>
2024-09-10 09:18:19 +0900

    test: LottoNumberTest 검증 케이스 추가

--- src/test/kotlin/lotto/LottoNumberTest.kt
+++ src/test/kotlin/lotto/LottoNumberTest.kt
@@ -2,13 +2,19 @@ package lotto
 
 import org.assertj.core.api.Assertions.assertThatIllegalStateException
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
 
 class LottoNumberTest {
 
     @Test
     fun `로또의 숫자는 1-45이다`() {
         assertThatIllegalStateException()
+            .isThrownBy { LottoNumber(0) }
+            .withMessage("로또의 숫자는 1-45 여야 합니다.")
+        assertThatIllegalStateException()
             .isThrownBy { LottoNumber(46) }
             .withMessage("로또의 숫자는 1-45 여야 합니다.")
+        assertDoesNotThrow { LottoNumber(1) }
+        assertDoesNotThrow { LottoNumber(45) }
     }
 }
